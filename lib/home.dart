0d294a704b6fdde680736287e7ccd470587dc880
magalhaesmth <aaa>
2023-03-02 22:36:35 -0300

    Validando CNPJ

--- lib/home.dart
+++ lib/home.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   Home({Key? key}) : super(key: key);
   var campoCPF = TextEditingController();
+  var campoCNPJ = TextEditingController();
 
+/*
   String validarCPF(String cpfCompleto) {
     if (!cpfCompleto.contains('.')) return 'CPF deve conter "."!';
     if (!cpfCompleto.contains('-')) return 'CPF deve conter "-"!';
@@ -64,11 +66,66 @@ class Home extends StatelessWidget {
 
     return 'CPF Válido!';
 }
+*/
+//===================Validar CNPJ agora=======================
 
-// Retiramos os IFS desnecessarios, adicionando la para cima
-// Sempre validando no app se está mudando mesmo
-// Retirar variaveis não utilizadas
-// Criar variaveis temporarias para facilitar a leitura do codigo
+  String validarCNPJ(String cnpjCompleto) {
+    if (!cnpjCompleto.contains('.')) return 'CNPJ deve conter "."!';
+    if (!cnpjCompleto.contains('-')) return 'CNPJ deve conter "-"!';
+    if (!cnpjCompleto.contains('/')) return 'CNPJ deve conter "/"!';
+    if (cnpjCompleto.length != 18) return 'CNPJ deve conter 18 caracteres!';
+
+    var cpfLimpo = cnpjCompleto.replaceAll('.', '').replaceAll('-', '').replaceAll('/', '');
+    var cpfListaNumeros = cpfLimpo
+        .substring(0, 12)
+        .split('')
+        .map<int>((e) => int.parse(e))
+        .toList();
+    var ehNumerosIguais = true;
+    var primeiroDigito = int.parse(cpfLimpo.substring(12, 13));
+    var segundoDigito = int.parse(cpfLimpo.substring(13, 14));
+
+    for (var i = 1; i < cpfListaNumeros.length; i++) {
+      var numeroAnterior = cpfListaNumeros[i - 1];
+      var numeroAtual = cpfListaNumeros[i];
+
+      if (numeroAnterior != numeroAtual) {
+        ehNumerosIguais = false;
+        break;
+      }
+    }
+    if (ehNumerosIguais) return "CNPJ deve possuir números diferentes!";
+
+    var peso = 5;
+    var digitoCalculado = 0;
+
+    for (var n in cpfListaNumeros) {
+      digitoCalculado = peso * n + digitoCalculado;
+      peso--;
+      if (peso == 1){
+        peso = 9;
+      }
+    }
+
+    digitoCalculado = 11 - (digitoCalculado % 11);
+    if (digitoCalculado > 9) digitoCalculado = 0;
+    if (primeiroDigito != digitoCalculado) return "Primeiro digito incorreto!";
+    cpfListaNumeros.add(digitoCalculado);
+    peso = 6;
+    digitoCalculado = 0;
+    for (var n in cpfListaNumeros) {
+      digitoCalculado += peso * n;
+      peso--;
+        if (peso == 1){
+        peso = 9;
+      }
+    }
+    digitoCalculado = 11 - (digitoCalculado % 11);
+    if (digitoCalculado > 9) digitoCalculado = 0;
+    if (segundoDigito != digitoCalculado) return "Segundo digito incorreto!";
+
+    return 'CPF Válido!';
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -79,13 +136,13 @@ class Home extends StatelessWidget {
           children: [
             TextField(
               decoration: const InputDecoration(
-                  label: Text('CPF'), hintText: 'Informe o seu CPF'),
-              controller: campoCPF,
+                  label: Text('CNPJ'), hintText: 'Informe o seu CNPJ'),
+              controller: campoCNPJ,
             ),
             ElevatedButton(
               child: const Text('Verificar CPF'),
               onPressed: () {
-                var resultado = validarCPF(campoCPF.text);
+                var resultado = validarCNPJ(campoCNPJ.text);
                 showDialog(
                     context: context,
                     builder: (BuildContext context) {
