a7636a1dc705179feb85badf49d6b414bdff15de
magalhaesmth <--global>
2023-03-09 22:28:48 -0300

    Validando primeiro digito

--- lib/entidade/validar_CPF.dart
+++ lib/entidade/validar_CPF.dart
@@ -1,13 +1,11 @@
-class ValidarCPF{
-
-  bool ehVazio (String cpf){
+class ValidarCPF {
+  bool ehVazio(String cpf) {
     if (cpf.isEmpty) throw Exception('CPF não pode ser vazio!');
     return true;
   }
 
-  ehTamanhoCorreto (String cpf) {
-    if(cpf.length != 14) 
-      throw Exception('CPF deve possuir 14 caracteres');
+  ehTamanhoCorreto(String cpf) {
+    if (cpf.length != 14) throw Exception('CPF deve possuir 14 caracteres');
     return true;
   }
 
@@ -23,7 +21,28 @@ class ValidarCPF{
     var cpfSemMascara = cpfCompleto.replaceAll('.', '').replaceAll('-', '');
     var cpfSemDigito = cpfSemMascara.substring(0, 9);
     List<String> listaCaracteres = cpfSemDigito.split('');
-    List<int> listaNumeros = listaCaracteres.map<int>((elemento)=>int.parse(elemento)).toList();
+    List<int> listaNumeros =
+        listaCaracteres.map<int>((elemento) => int.parse(elemento)).toList();
     return listaNumeros;
+  }
+
+  validarPrimeiroDigito(String cpf) {
+    var peso = 10;
+    var digitoCalculado = 0;
+    var cpfLimpo = cpf.replaceAll('.', '').replaceAll('-', '');
+    var primeiroDigito = int.parse(cpfLimpo.substring(9, 10));
+
+    for (var n in gerarListaNumero(cpf)) {
+      digitoCalculado += peso * n;
+    }
+
+    digitoCalculado = 11 - (digitoCalculado % 11);
+    if (primeiroDigito > 9) digitoCalculado = 0;
+
+    return primeiroDigito;
+  }
+
+  validarSegundoDigito(String cpf){
+    
   }
 }
--- test/validar_CPF_test.dart
+++ test/validar_CPF_test.dart
@@ -8,28 +8,29 @@ import 'package:aulaum/entidade/validar_CPF.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 //CPF não pode ser vazio
-//
 
 void main() {
-  test(
-    'CPF não pode ser vazio',
-    (){
-      String cpf = '';
-      var validarCPF = ValidarCPF();
-      expect(()=>validarCPF.ehVazio(cpf), throwsException);
-    }
-  );
+  test('CPF não pode ser vazio', () {
+    String cpf = '';
+    var validarCPF = ValidarCPF();
+    expect(() => validarCPF.ehVazio(cpf), throwsException);
+  });
 
-  test(
-    'CPF deve possuir 14 caracteres',
-    (){
-      String cpf = '111.111.111-11';
-      var validarCPF = ValidarCPF();
-      expect(()=>validarCPF.ehTamanhoCorreto(cpf), true);
-      cpf = '1111';
-      expect(()=> validarCPF.ehTamanhoCorreto(cpf), throwsException);
-    }
-  );
+//Deve possuir o formato correto
+//CPF deve possuir .
+//CPF deve possuir -
+//CPF sem máscara e sem dígito deve possuir 9 caracteres
+//deve calcular o primeiro dígito
+//deve calcular o segundo dígito
+//deve validar os dígitos
+
+  test('CPF deve possuir 14 caracteres', () {
+    String cpf = '111.111.111-11';
+    var validarCPF = ValidarCPF();
+    expect(validarCPF.ehTamanhoCorreto(cpf), true);
+    cpf = '1111';
+    expect(() => validarCPF.ehTamanhoCorreto(cpf), throwsException);
+  });
 
   test('CPF deve possuir formato correto', () {
     String cpf = '111.111.111-11';
@@ -37,7 +38,7 @@ void main() {
     expect(validarCPF.ehFormatoCorreto(cpf), true);
     cpf = '1111';
     expect(() => validarCPF.ehFormatoCorreto(cpf), throwsException);
-});
+  });
 
   test('CPF sem máscara e sem digito devem possuir 9 caracteres', () {
     String cpf = '123.456.789-10';
@@ -45,5 +46,11 @@ void main() {
     List<int> listaNumerosCPF = validarCPF.gerarListaNumero(cpf);
     expect(listaNumerosCPF.length, 9);
     cpf = '1111';
+  });
+
+  test('Primeiro digito correto', () {
+    String cpf = '123.456.789-10';
+    var validarCPF = ValidarCPF();
+    expect(validarCPF.validarPrimeiroDigito(cpf), 1);
   });
 }
